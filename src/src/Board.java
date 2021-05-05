84d12fa89a054d8bcb28337e79f10e7eef2cc384
Anas <anasa@kth.se>
2021-05-05 16:31:21 +0200

    "open two classes"

---
+++ src/src/Board.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Board extends JFrame {
+
+
+    Board(){
+
+    }
+
+}
---
+++ src/src/Memory.java
@@ -0,0 +1,11 @@
+
+/*
+Memory will include the main method.
+ */
+
+public class Memory {
+
+    public static void main(String[] args) {
+
+    }
+}
